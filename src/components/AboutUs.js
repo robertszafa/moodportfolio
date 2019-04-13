b98c5e021850b98f808c354b36e39b07212da7f5
sgpbeatt <sgpbeatt@liverpool.ac.uk>
2019-04-13 11:55:48 +0100

    updating phills bits

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { } from 'react-bootstrap'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 export default class AboutUs extends Component 
 {
@@ -7,6 +8,23 @@ export default class AboutUs extends Component
         return( 
         <div>
         
+            <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>
+                This is a simple hero unit, a simple jumbotron-style component for calling
+                extra attention to featured content or information.
+            </p>
+            <p>
+                <Button variant="primary">Learn more</Button>
+            </p>
+            </Jumbotron>;
+
+
+
+
+
+
+
         </div>
         )
     } 
