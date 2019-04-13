8da638205691116ccd144a1ea6c57c5c8dade10b
sgpbeatt <sgpbeatt@liverpool.ac.uk>
2019-04-13 12:23:48 +0100

    Finished AboutUs page layout.  Still need to complete descriptions

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,28 +4,62 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 
 export default class AboutUs extends Component 
 {
-    render() {
+    render() 
+    {
         return( 
-        <div>
-        
-            <Jumbotron>
-            <h1>Hello, world!</h1>
-            <p>
-                This is a simple hero unit, a simple jumbotron-style component for calling
-                extra attention to featured content or information.
-            </p>
-            <p>
-                <Button variant="primary">Learn more</Button>
-            </p>
-            </Jumbotron>;
+            <div>
+                <Jumbotron>
+                
+                    <div class="page-header">
+                        <h1 class="text-center">
+                            Meet the team
+                        </h1>
+                    </div>
 
+                    <h3>
+                        Rob
+                    </h3>
+                    <p>
+                        Rob is a big nerd and has completed most of the site
+                    </p>
 
+                    <h3>
+                        Thep
+                    </h3>
+                    <p>
+                        Thep has just come back from Devon and is helping Phill get to grips with the site
+                    </p>
 
+                    <h3>
+                        Phill
+                    </h3>
+                    <p>
+                        Phill is enjoying learning react and implementing features
+                    </p>
 
+                    <h3>
+                        Rahul
+                    </h3>
+                    <p>
+                        Rahul is the AI master of this project
+                    </p>
 
+                    <h3>
+                        Gareth
+                    </h3>
+                    <p>
+                        Gareth can do everything 
+                    </p>
 
+                    <h3>
+                        Matt
+                    </h3>
+                    <p>
+                        Matt is off on his jollies
+                    </p>
 
-        </div>
+                </Jumbotron>;
+            </div>
         )
     } 
 }
