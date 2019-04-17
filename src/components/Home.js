8acdbcfbed9da80dde4a41669c6414b2c5f9a718
MJHarding <33782780+MJHarding@users.noreply.github.com>
2019-04-17 15:33:59 +0100

    Set up two sections for home componenets to be added

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,20 @@ export default class Home extends React.Component {
     render() {
       return (
           <div>
-              <h1>Hello! You are: {this.emotion}</h1>
+            <div className = "text-center homeGraph">
+                <h1>Hello! You are: {this.emotion}</h1>
+                <div id = "graphContainer">
+                    "ENTER GRAPH COMPONENT css for color in app.css"
+                </div>
+            </div>
+
+            <div className = "text-center homeSecondary">
+                "enter secondary component here, css for color in app.css"
+            </div>
+
           </div>
+
+
       );
     }
   }
