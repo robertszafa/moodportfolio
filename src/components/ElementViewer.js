994047661f2b2fad22562cf4b57f6973544288c1
DevGareth <47175332+DevGareth@users.noreply.github.com>
2019-04-18 10:16:47 +0100

    Stats Page
    Not sized correctly yet, just has all of the components.

---
+++ src/components/ElementViewer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import '../stylesheet/stats.css';
+
+//should take an array of data. Need to receive click event too
+export default class ElementViewer extends React.Component{
+	constructor (props) {
+		super(props);
+		
+	}
+	
+	renderCard(stamp, url, tags, emo){
+		return (
+		<ElementCard timestamp = {stamp} imageurl = {url} tags = {tags} emotion={emo} />
+		);
+	}
+	
+	render () {
+		return (
+			<div>
+				renderCard();
+			</div>
+		);
+	}
+}
+
+function ElementCard(props) {
+	return (
+			<Card style={{ width: '100%' }} className='card'>
+			<Card.Img variant="top" src=""/>
+			<Card.Body>
+				<Card.Title>{this.props.emotion}<br/>{this.props.timestamp}</Card.Title>
+				{/* Tags */}
+				<Button variant="primary">"Change Emotion"</Button>
+				<Button variant="primary">"Delete"</Button>
+			</Card.Body>
+		</Card>
+	);
+}
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,104 +1,302 @@
 import React from 'react';
-import { Bar, Line, Pie } from 'react-chartjs-2';
-import {apiMoodportfolio} from '../App'
+import ReactDOM from 'react-dom';
+import GraphPlotter from './GraphPlotter.js';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';  //not clear why i don't need these
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import NodeViewer from './NodeViewer.js';
+import ElementViewer from './ElementViewer.js';
 
-//The Graph component. Draws pie, line and bar charts.
+//The graph and its menus. Also has the method for getting data for the graphs based on the selected options.
 export default class Graph extends React.Component {
-	constructor(props) {	
+	constructor (props) {
 		super(props);
-
 		this.state = {
-			type: props.type,
-			data: props.data,
+			startDate: getDayStart(Date.now()),
+			endDate: getDayEnd(Date.now()),
+			selectedTime: 1, //Day = 1, Week = 2, Month  = 3
+			selectedGraph: 3, //Bar = 1, Line = 2, Pie = 3, Radio = 4 (can't be selected)
+			graphData: {},
 			indexClicked: -1
-		};
+		}
 		
-		this.handleClick = this.handleClick.bind(this);
+		this.handleTimeClick = this.handleTimeClick.bind(this);
+		this.handleTypeClick = this.handleTypeClick.bind(this);
+		this.handleBackClick = this.handleBackClick.bind(this);
+		this.handleForwardClick = this.handleForwardClick.bind(this);
+		this.handleNodeClick = this.handleNodeClick.bind(this);
 	}
-
-	handleClick(e){
-		//console.log(this.state.indexClicked); //for testing
+	
+	componentWillMount(){
+		this.GetGraphData();
+	}
+	
+	//Get Graph Data
+	
+	GetGraphData(){
+		//API call here to get the data
+		
+		//need to do various stuff based on:
+		//timeSpan
+		//over time, emotion or tag 
+		
+		//switch over graphOption
+		this.setState ({
+				graphData: {
+					label: 'Pie data',
+					labels: [
+						'Anger',
+						'Contempt',
+						'Disgust',
+						'Fear',
+						'Joy',
+						'Surprise',
+						'Sadness',
+						'Neutral'
+					],
+					datasets: [{
+						data: [20, 30, 15, 10, 20, 9, 8, 17],
+						backgroundColor: [
+							'#d270d3',
+							'#fb7821',
+							'#e83e17',
+							'#bfc0ee',
+							'#9f9e26',
+							'#700846',
+							'#771aab',
+							'#e24b5a'
+						],
+						hoverBackgroundColor: [
+							'#043d7e',
+							'#beafa9',
+							'#4fc690',
+							'#667559',
+							'#d29e81',
+							'#46bbe9',
+							'#13744c',
+							'#9125d5'
+						]
+					}]
+				}
+		});
+	}
+	
+	//Button handling functions
+	
+	handleTimeClick(o){
+		//if day then don't subtract
+		var d;
+		if (o == 1){
+			d = this.state.endDate;
+		} else {
+			d = changeDate(o,this.state.endDate,-1);
+		}
+		
+		this.setState({
+			selectedTime: o,
+			startDate: d
+		}); //may not need to store this at all but keeping for now in hopes of fixing the buttongroups
+	}
+	
+	handleTypeClick(o){
+		this.setState({selectedGraph: o});
+	}
+	
+	handleBackClick(){
+		this.setState({
+			startDate: changeDate(this.state.selectedTime, this.state.startDate, -1),
+			endDate: changeDate(this.state.selectedTime, this.state.endDate, -1)
+		});
+	}
+	
+	handleForwardClick(){
+		this.setState({
+			startDate: changeDate(this.state.selectedTime, this.state.startDate, 1),
+			endDate: changeDate(this.state.selectedTime, this.state.endDate, 1)
+		});
+	}
+	
+	handleNodeClick(e){
 		var index = -1;
 		
-		//if event fires but nothing selected
+		//if event fires and something is selected.
 		if (e.length !== 0){
 			index = e[0]._index;
 		}
+		console.log(index);
 		//set the index clicked
 		this.setState({indexClicked: index});
 	}
 	
-	renderBar(){
+	render () {
+		console.log(this.state.selectedGraph);
+		//do different stuff based on if data is ready or not
+		
+		var j;
+		if (this.state.indexClicked != -1){
+			j = <NodeViewer emotion={"Happy"} timestamp={Date.now()}/>;
+		}
+		
 		return (
-			<BarGraph
-			value = {this.state.data}
-			getIndex = {(e) => this.handleClick(e)}
-			/>
+		<div className='text-center'>
+			{/* Time unit menu */}
+			<TimeMenu onClick = {this.handleTimeClick}/>
+			{/* Date menu */}
+			<DateSelector startDate={this.state.startDate} endDate={this.state.endDate} timeCode={this.state.selectedTime} onBackClick={this.handleBackClick} onForwardClick={this.handleForwardClick}/>
+			{/* Time unit menu */}
+			<GraphPlotter type = {this.state.selectedGraph} data = {this.state.graphData} onClick={this.handleNodeClick}/>
+			{/* Graph type menu */}
+			<GraphMenu onClick = {this.handleTypeClick}/>
+			{/* Node Viewer */}
+			{j}
+		</div>
 		);
 	}
-	
-	renderLine(){
-		return (
-			<LineGraph
-			value = {this.state.data}
-			getIndex = {(e) => this.handleClick(e)}
-			/>
-		);
+}
+
+class DateSelector extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.handleBackClick = this.handleBackClick.bind(this);
+		this.handleForwardClick = this.handleForwardClick.bind(this);
 	}
 	
-	renderPie(){
-		return (
-			<PieGraph
-			value = {this.state.data}
-			getIndex = {(e) => this.handleClick(e)}
-			/>
-		);
+	handleBackClick(o) {
+		this.props.onBackClick(o);
 	}
 	
-	render() {
+	handleForwardClick(o) {
+		this.props.onForwardClick(o);
+	}
+	
+	render () {
+		//compare future date with current and decide whether to render end button.
+		//if (changeDate(this.props.timeCode,this.props.endDate,1) > 
+		
+		var dateText;
+		if (this.props.timeCode == 1){
+			dateText = " " + formatDate(this.props.endDate) + " ";
+		} else {
+		dateText = (" " + formatDate(this.props.startDate) + " - " + formatDate(this.props.endDate) + " ");
+			//dateText = "ERROR";
+			//dateText= " " + formatDate(this.props.startDate)} <b> " - " </b> formatDate(this.props.endDate) + " ";
+			//dateText = {" " + formatDate(this.props.startDate)} + {<b>" - "</b>} + {formatDate(this.props.endDate) + " "}
+		}
+		
 		return (
-			<div>
-				<h1>Graph</h1>
-				{this.renderPie()}
+			<div className='date-selector'>
+				<Button variant="primary" onClick={this.handleBackClick}>{"<"}</Button>
+				{dateText}
+				<Button variant="primary" onClick={this.handleForwardClick}>{">"}</Button>
 			</div>
 		);
 	}
+}
+
+class TimeMenu extends React.Component {
 	
-
+	constructor(props){
+		super(props)
+		this.handleClick = this.handleClick.bind(this)
+	}
+	
+	handleClick(o) {
+		this.props.onClick(o);
+	}
+	
+	renderButton(n, v){
+		return (
+			<MenuButton
+			name={n}
+			onClick = {() => this.handleClick(v)}
+			/>
+		);
+	}
+	
+	render () {
+		return (
+			<ButtonGroup className="time-menu" size="sm">
+				{this.renderButton("Day",1)}
+				{this.renderButton("Week",2)}
+				{this.renderButton("Month",3)}
+			</ButtonGroup>
+		);
+	}
 }
 
-function PieGraph(props) {
+function MenuButton(props) {
 	return (
-		<div>
-			<h2>Pie Example</h2>
-			<Pie 
-			data = {props.value} 
-			getElementAtEvent = {(element) => props.getIndex(element)}
-			/>
-		</div>
+			<Button
+			variant = "secondary"
+			onClick={props.onClick}
+			>
+			{props.name}
+			</Button>
 	);
 }
 
-function LineGraph(props) {
-	return (
-		<div>
-			<h2>Line Example</h2>
-			<Line 
-			data = {props.value}
-			getElementAtEvent = {(element) => props.getIndex(element)}
+class GraphMenu extends React.Component {
+	
+	constructor(props){
+		super(props)
+		this.handleClick = this.handleClick.bind(this)
+	}
+	
+	handleClick(o) {
+		this.props.onClick(o);
+	}
+	
+	renderButton(n, v){
+		return (
+			<MenuButton
+			name={n}
+			onClick = {() => this.handleClick(v)}
 			/>
-		</div>
+		);
+	}
+	
+	render () {
+		return (
+			<ButtonGroup className="graph-menu" size="sm">
+				{this.renderButton("Bar",1)}
+				{this.renderButton("Line",2)}
+				{this.renderButton("Pie",3)}
+			</ButtonGroup>
+		);
+	}
+}
+
+function changeDate(timecode, date, modifier) {
+		var d = new Date(date);
+		switch (timecode){
+			case 1:
+				/* Day */
+				return new Date(d.setDate(d.getDate() + (1 * modifier)));
+			case 2:
+				/* Week */
+				return new Date(d.setDate(d.getDate() + (7 * modifier)));
+			default:
+				/* Month */
+				return new Date(d.setMonth(d.getMonth() + (1 * modifier)));
+		}
+}
+
+function formatDate(date) {
+	var d = new Date(date);
+	return (
+		d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear()
 	);
 }
 
-function BarGraph(props) {
-	return (
-		<div>
-			<h2>Bar Example</h2>
-			<Bar 
-			data = {props.value}
-			getElementAtEvent = {(element) => props.getIndex(element)}
-			/>
-		</div>
-	);
+function getDayEnd(date){
+	var d = new Date(date);
+	d.setHours(23,59,59,999);
+	return new Date(d);
+}
+
+function getDayStart(date){
+	var d = new Date(date);
+	d.setHours(0,0,0,0);
+	return new Date(d);
 }
