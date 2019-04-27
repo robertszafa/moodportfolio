dbf69e437a8fa032a9b8f26c28320427128529ee
DevGareth <47175332+DevGareth@users.noreply.github.com>
2019-04-27 18:23:58 +0100

    Revert "Update Graph.js"
    This reverts commit 1d4cc3bfeb07f86b45836590fd6c07378dfc8b24.

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -21,15 +21,13 @@ export default class Graph extends React.Component {
 			selectedGraph: 2, //Bar = 1, Line = 2, Pie = 3, Radio = 4 (can't be selected). Have left bar in for now.
 			graphData: {},
 			graphOptions: {},
-			indexClicked: -1, 
+			indexClicked: -1,
 			photos: [],
 			indexLabels: []
 		}
-		
+
 		//this.photos = new Array(); //other way to set up the photos array if not meant to be state.
-		
-		this.baseColours = ['#d270d3','#fb7821','#e83e17','#bfc0ee','#9f9e26','#700846','#771aab','#e24b5a'];
-		this.hoverColours = ['#043d7e','#beafa9','#4fc690','#667559','#d29e81','#46bbe9','#13744c','#9125d5'];
+
 		this.displayEmotions = ['fear','anger','contempt','disgust','sadness','neutral','surprise','happiness'];
 		this.dbEmotions = ['anger','contempt','disgust','fear','happiness','neutral','sadness','surprise'];
 		this.handleTimeClick = this.handleTimeClick.bind(this);
@@ -38,12 +36,12 @@ export default class Graph extends React.Component {
 		this.handleForwardClick = this.handleForwardClick.bind(this);
 		this.handleNodeClick = this.handleNodeClick.bind(this);
 	}
-	
+
 	componentWillMount(){
 		this.GetPhotos(this.state.startDate,this.state.endDate);
 		//this.testDataTime(this.state.selectedTime,this.state.startDate,this.state.endDate);
 	}
-	
+
 	// Gets Photos from server and then stuff
 	// Uses startDate, endDate
 	GetPhotos(start, end){
@@ -59,7 +57,7 @@ export default class Graph extends React.Component {
 		this.setState({
 			photos: []
 		});
-		
+
 		fetch(apiMoodportfolio + '/EmotionsQuery', {
 					method: "GET",
 					mode: "cors",
@@ -92,10 +90,10 @@ export default class Graph extends React.Component {
 	}
 
 	SetGraphData(graphCode,start,end){
-		
+
 		//if chart is line then need to average out the data, otherwise don't.
 		//extract data from photos
-		
+
 		/*
 		console.log("PIE CHART");
 		console.log(start + "\n" + end);
@@ -115,29 +113,25 @@ export default class Graph extends React.Component {
 
 		//if graph is over time
 		if (graphCode === 2){
-			if (this.props.menuOption === 1){
-				this.SetGraphData_OverTime(emotionProb,timestamp,start,end);
-			} else {
-				this.setGraphData_Emotions(emotionProb,timestamp,start,end);
-			}
+			this.SetGraphData_OverTime(emotionProb,timestamp,start,end);
 		} else {
 			this.SetGraphData_Overall(emotionProb);
 		}
 
 	}
-	
+
 	SetGraphData_Overall(emotionProbs){
 		this.state.graphOptions = {};
 		//var emotionP = emotionProbs;
 		//loop through the emotions and count total of each
 		let emotionCount = new Array(this.dbEmotions.length).fill(0);
-		
+
 		let i, j;
 		for (i = 0; i < emotionProbs.length; i++){
 			for (j = 0; j < this.dbEmotions.length; j++){
 				if (emotionProbs[i].includes(this.dbEmotions[j])){
 					emotionCount[GetEmotionIndex(getEmotionFromString(emotionProbs[i]))] ++;
-				}	
+				}
 			}
 		}
 
@@ -148,16 +142,34 @@ export default class Graph extends React.Component {
 				labels: this.displayEmotions,
 				datasets: [{
 					data: emotionCount,
-					backgroundColor: this.baseColours,
-					hoverBackgroundColor: this.hoverColours,
+					backgroundColor: [
+						'#11ff00',
+						'#ff0011',
+						'#ee00ff',
+						'#80027a',
+						'#1003ff',
+						'#06ffb4',
+						'#ff7206',
+						'#fff700'
+					],
+					hoverBackgroundColor: [
+						'#043d7e',
+						'#beafa9',
+						'#4fc690',
+						'#667559',
+						'#d29e81',
+						'#46bbe9',
+						'#13744c',
+						'#9125d5'
+					]
 				}]
 			}
 		});
 	}
 
 	SetGraphData_OverTime(emotionProbs,timestamp,startdate,enddate){
-		
-	let timeUnit = this.state.selectedTime;	
+
+	let timeUnit = this.state.selectedTime;
 	let datetimeLabels = [];
 	datetimeLabels.length = 0;
 	let i = 0, j = 0;
@@ -165,9 +177,9 @@ export default class Graph extends React.Component {
 	let thedata = [];
 	thedata.length = 0;
 	let stepSize, timeValue, minDate, maxDate, lastUnit, missingUnits, timeFormat, numTicks, changeDateUnit;
-	
+
 	let emotionCompare = [];
-	
+
 	switch(timeUnit){
 		case 1:
 			changeDateUnit = 0;
@@ -178,7 +190,7 @@ export default class Graph extends React.Component {
 			maxDate = enddate;
 			numTicks = 24;
 			break;
-			
+
 		case 2:
 			changeDateUnit = 1;
 			timeFormat = 'MMM DD';
@@ -188,19 +200,19 @@ export default class Graph extends React.Component {
 			maxDate = changeDate(changeDateUnit,enddate,-1);
 			numTicks = 7;
 			break;
-			
+
 		case 3:
 			changeDateUnit = 1;
 			timeFormat = 'MMM DD';
 			stepSize = 'day';
 			timeValue = 3;
 			minDate = startdate;
-			maxDate = changeDate(1,enddate,-1); //need to check this			
+			maxDate = changeDate(1,enddate,-1); //need to check this
 			break;
 			default:
 			console.log("broke" + timeUnit);
 	}
-	
+
 	if (timeUnit === 3){
 		while (d < enddate){
 			datetimeLabels.push(d);
@@ -216,7 +228,7 @@ export default class Graph extends React.Component {
 	i = 0;
 	lastUnit = getUnitQuantity(startdate,changeDateUnit);
 	missingUnits = 0;
-	
+
 	while (i < timestamp.length){
 		//check if part of the current time
 		if (getUnitQuantity(timestamp[i],changeDateUnit) === lastUnit){
@@ -235,7 +247,7 @@ export default class Graph extends React.Component {
 			//find most prominent emotion, get its number, push it
 			if (emotionCompare.length > 0){
 				console.log("PUSHING PROMINENT EMOTION");
-				thedata.push(GetEmotionIndex(GetModeEmotion(emotionCompare)) + 1);
+				thedata.push(GetEmotionIndex(GetModeEmotion(emotionCompare)));
 			}
 			//clear emotionCompare
 			emotionCompare = [];
@@ -247,13 +259,13 @@ export default class Graph extends React.Component {
 		}
 		i++;
 	}
-	
+
 	if (timestamp.length > 0){
-		
+
 		//if leftover data in emotionCompare then push
 		if (emotionCompare.length > 0){
 			console.log("PUSHING DATA TO thedata");
-			thedata.push(GetEmotionIndex(GetModeEmotion(emotionCompare)) + 1);
+			thedata.push(GetEmotionIndex(GetModeEmotion(emotionCompare)));
 			console.log(thedata);
 		}
 
@@ -262,16 +274,16 @@ export default class Graph extends React.Component {
 		if (maxUnit === 0){
 			maxUnit = 23;
 		}
-		
+
 		let remainingNulls = maxUnit - getUnitQuantity(timestamp[timestamp.length - 1],changeDateUnit);
 		console.log("PUSHING " + remainingNulls + " remaining nulls.");
 		for (i = 0; i < remainingNulls; i++){
 			thedata.push(null);
 		}
 	}
-		
+
 	console.log("END OF NEW CODE");
-		
+
 	let yLabels = {
 		0: '',
 		1: 'Anger',
@@ -291,7 +303,7 @@ export default class Graph extends React.Component {
 	console.log(datetimeLabels);
 	this.setState({
 		indexLabels: datetimeLabels,
-		
+
 		graphData: {
 			label: 'Emotions Over Time',
 			labels: datetimeLabels,
@@ -299,23 +311,20 @@ export default class Graph extends React.Component {
 				label: "Emotions",
 				data: thedata,
 				borderColor: '#e24b5a',
-				backgroundColor: this.baseColours,
-				hoverBackgroundColor: this.hoverColours,
-				fill: false,
-				spanGaps: true //dunno whether to use this.
+				fill: false
 			}]
 		},
-		
+
 		graphOptions: {
-			
+
 			legend: {
 				display: false,
 			},
-			
+
 			tooltips: {
 				enabled: false,
 			},
-			
+
 			scales: {
 				yAxes: [{
 					interval:0,
@@ -354,225 +363,12 @@ export default class Graph extends React.Component {
 	});
 }
 
-setGraphData_Emotions(emotionProbs,timestamp,startdate,enddate){
-	
-	let timeUnit = this.state.selectedTime;	
-	let datetimeLabels = [];
-	datetimeLabels.length = 0;
-	let i = 0, j = 0;
-	let d = startdate;
-	let thedata = [];
-	thedata.length = 0;
-	let tempdata = [];
-	let stepSize, timeValue, minDate, maxDate, lastUnit, missingUnits, timeFormat, numTicks, changeDateUnit;
-	
-	let emotionCompare = [];
-	
-	switch(timeUnit){
-		case 1:
-			changeDateUnit = 0;
-			timeFormat = 'hA';
-			stepSize = 'hour'
-			timeValue = 3;
-			minDate = startdate;
-			maxDate = enddate;
-			numTicks = 24;
-			break;
-			
-		case 2:
-			changeDateUnit = 1;
-			timeFormat = 'MMM DD';
-			stepSize = 'day'
-			timeValue = 1;
-			minDate = startdate;
-			maxDate = changeDate(changeDateUnit,enddate,-1);
-			numTicks = 7;
-			break;
-			
-		case 3:
-			changeDateUnit = 1;
-			timeFormat = 'MMM DD';
-			stepSize = 'day';
-			timeValue = 3;
-			minDate = startdate;
-			maxDate = changeDate(1,enddate,-1); //need to check this			
-			break;
-			default:
-			console.log("broke" + timeUnit);
-	}
-	
-	if (timeUnit === 3){
-		while (d < enddate){
-			datetimeLabels.push(d);
-			d = changeDate(1,d,1);
-		}
-	} else {
-		for (i = 0; i < numTicks; i++){
-			datetimeLabels.push(d);
-			d = changeDate(changeDateUnit,d,1);
-		}
-	}
+setGraphData_Emotions(){
 
-
-	let emote;
-	for (emote = 0; emote < this.displayEmotions.length; emote++){
-		i = 0;
-		lastUnit = getUnitQuantity(startdate,changeDateUnit);
-		missingUnits = 0;
-		tempdata = [];
-		while (i < timestamp.length){
-			//check if part of the current time
-			if (getUnitQuantity(timestamp[i],changeDateUnit) === lastUnit){
-				console.log("PUSHING" + emotionProbs[i] + " TO COMPARISON.");
-				emotionCompare.push(emotionProbs[i]);
-			} else {
-				//if new time
-				missingUnits = getUnitQuantity(timestamp[i],changeDateUnit) - lastUnit - 1;
-				console.log(missingUnits + " missing units found. " + timestamp[i] + "	" + changeDateUnit);
-				if (missingUnits > 1 || (missingUnits > -1 && i === 0)){
-					if (i === 0) {missingUnits++;}
-					console.log("ADDING NULLS * " + missingUnits + "from: " + timestamp[i]);
-					for (j = 0; j < missingUnits; j++){
-						tempdata.push(null);
-					}
-				}
-				//push count of emotion
-				if (emotionCompare.length > 0){
-					console.log("PUSHING PROMINENT EMOTION");
-					tempdata.push(emotionCompare.length);
-				}
-				//clear emotionCompare
-				emotionCompare = [];
-				//add the current element to emotionCompare
-				console.log("PUSHING CURRENT ELEMENT TO COMPARISON");
-				emotionCompare.push(emotionProbs[i]);
-				//set the current time being looked at
-				lastUnit = getUnitQuantity(timestamp[i],changeDateUnit);
-			}
-			i++;
-		}
-		
-		if (timestamp.length > 0){
-			
-			//if leftover data in emotionCompare then push
-			if (emotionCompare.length > 0){
-				console.log("PUSHING DATA TO thedata");
-				tempdata.push(emotionCompare.length);
-			}
-
-			//add remaining nulls
-			let maxUnit = getUnitQuantity(enddate,changeDateUnit);
-			if (maxUnit === 0){
-				maxUnit = 23;
-			} else if (maxUnit === 1){
-				maxUnit = getUnitQuantity(changeDate(1,enddate,-1),changeDateUnit);
-			}
-			
-			let remainingNulls = maxUnit - getUnitQuantity(timestamp[timestamp.length - 1],changeDateUnit);
-			console.log("PUSHING " + remainingNulls + " remaining nulls. " + maxUnit + " - " + timestamp[timestamp.length-1]);
-			for (i = 0; i < remainingNulls; i++){
-				tempdata.push(null);
-			}
-		}
-		
-		thedata.push(tempdata);
-	}
-	
-	let yLabels = {
-		0: '',
-		1: 'Anger',
-		2: 'Contempt',
-		3: 'Disgust',
-		4: 'Fear',
-		5: 'Sadness',
-		6: 'Neutral',
-		7: 'Surprise',
-		8: 'Happiness'
-	}
-	/*
-	console.log("LINE CHART");
-	console.log("Date Time Labels");
-	console.log(datetimeLabels);
-	*/
-	console.log(datetimeLabels);
-	this.setState({
-		indexLabels: datetimeLabels,
-		
-		graphData: {
-			label: 'Emotions Over Time',
-			labels: datetimeLabels,
-			datasets: [{
-				label: yLabels[1],
-				data: thedata[0],
-				borderColor: this.baseColours[0],
-				backgroundColor: this.baseColours[0],
-				hoverBackgroundColor: this.hoverColours[0],
-				fill: false,
-				spanGaps: true,
-			},
-			
-			{
-				label: yLabels[2],
-				data: thedata[1],
-				borderColor: this.baseColours[1],
-				backgroundColor: this.baseColours[1],
-				hoverBackgroundColor: this.hoverColours[1],
-				fill: false,
-				spanGaps: true,
-			},
-			
-			
-			
-			]
-		},
-		
-		graphOptions: {
-			
-			legend: {
-				display: true,
-			},
-			
-			tooltips: {
-				enabled: false,
-			},
-			
-			scales: {
-				yAxes: [{
-					interval:0,
-					unitStepSize: 1,
-					title: "Emotion",
-					ticks: {
-						beginAtZero: true,
-					},
-					min: 0,
-				}],
-				xAxes: [{
-					type: 'time',
-					time: {
-						unit: stepSize,
-						unitStepSize: timeValue,
-						max: maxDate,
-						min: minDate,
-						displayFormats: {
-							'millisecond': timeFormat,
-							'second': timeFormat,
-							'minute': timeFormat,
-							'hour': timeFormat,
-							'day': timeFormat,
-							'week': timeFormat,
-							'month': timeFormat,
-							'quarter': timeFormat,
-							'year': timeFormat,
-						}
-					}
-				}]
-			}
-		}
-	});
 }
 
 	//Button handling functions
-	
+
 	//When clicking Day/Week/Month
 	handleTimeClick(selectedUnit){
 		var current = new Date(this.state.currentDate);
@@ -595,12 +391,12 @@ setGraphData_Emotions(emotionProbs,timestamp,startdate,enddate){
 			default:
 				console.log("Wrong selected unit in handleTimeClick");
 		}
-		
+
 		this.setState({
 			selectedTime: selectedUnit,
 			startDate: start
 		}); //may not need to store this at all but keeping for now in hopes of fixing the buttongroups
-		
+
 		this.setState({
 			endDate: end
 		})
@@ -608,21 +404,21 @@ setGraphData_Emotions(emotionProbs,timestamp,startdate,enddate){
 		this.GetPhotos(start, end);
 
 	}
-	
+
 	//When clicking Over Time/Overall
 	handleTypeClick(o){
 		this.setState({selectedGraph: o});
 		this.GetPhotos(this.state.startDate, this.state.endDate);
 		//this.testDataTime(this.state.startDate,this.state.endDate);
 	}
-	
+
 	//When clicking < in date selector
 	handleBackClick(){
 		let selectedUnit = this.state.selectedTime;
 		let current = this.state.currentDate;
 		let start = this.state.startDate;
 		let end = this.state.endDate;
-		
+
 		switch (selectedUnit){
 			case 1: //day
 				current = changeDate(1,current,-1);
@@ -642,7 +438,7 @@ setGraphData_Emotions(emotionProbs,timestamp,startdate,enddate){
 			default:
 				console.log("Wrong SelectedUnit in back click");
 		}
-		
+
 		this.setState({
 			currentDate: current,
 			startDate: start,
@@ -652,14 +448,14 @@ setGraphData_Emotions(emotionProbs,timestamp,startdate,enddate){
 		this.GetPhotos(start, end);
 		//this.testDataTime(selectedUnit, start, end);
 	}
-	
+
 	//When clicking > in date selector
 	handleForwardClick(){
 		let selectedUnit = this.state.selectedTime;
 		let current = this.state.currentDate;
 		let start = this.state.startDate;
 		let end = this.state.endDate;
-		
+
 		switch (selectedUnit){
 			case 1: //day
 				current = changeDate(1,current,1);
@@ -679,21 +475,21 @@ setGraphData_Emotions(emotionProbs,timestamp,startdate,enddate){
 			default:
 				console.log();
 		}
-		
+
 		this.setState({
 			currentDate: current,
 			startDate: start,
 			endDate: end
 		});
-		
+
 		this.GetPhotos(start, end);
 		//this.testDataTime(this.state.selectedTime, start, end);
 	}
-	
+
 	//When clicking a node of the graph
 	handleNodeClick(e){
 		var index = -1;
-		
+
 		//if event fires and something is selected.
 		if (e.length !== 0){
 			index = e[0]._index;
@@ -702,21 +498,21 @@ setGraphData_Emotions(emotionProbs,timestamp,startdate,enddate){
 		//set the index clicked
 		this.setState({indexClicked: index});
 	}
-	
+
 	render () {
 		//do different stuff based on if data is ready or not
-		
+
 		let nodeView;
 		if (this.state.indexClicked !== -1){
 			//nodeView = <NodeViewer emotion={"Happy"} timestamp={Date.now()}/>;
-			nodeView = <NodeViewer nodeClicked={this.state.indexClicked} indexLabels={this.state.datetimeLabels} data={this.state.photos} graphType={this.state.selectedGraph} timeUnit={this.state.selectedTime} startDate={this.state.startDate} endDate={this.state.endDate} emotions={this.displayEmotions}/> 
+			nodeView = <NodeViewer nodeClicked={this.state.indexClicked} indexLabels={this.state.datetimeLabels} data={this.state.photos} graphType={this.state.selectedGraph} timeUnit={this.state.selectedTime} startDate={this.state.startDate} endDate={this.state.endDate} emotions={this.displayEmotions}/>
 		}
-		
+
 		//Have temporarily added a blank button above the time unit menu as wasn't working for some reason.
 		//Also added paragraph as otherwise top half of buttons won't click.
-		
+
 		return (
-		
+
 		<div className='text-center'>
 			<br/><br/>
 			{/* Time unit menu */}
@@ -740,19 +536,19 @@ class DateSelector extends React.Component {
 		this.handleBackClick = this.handleBackClick.bind(this);
 		this.handleForwardClick = this.handleForwardClick.bind(this);
 	}
-	
+
 	handleBackClick(o) {
 		this.props.onBackClick(o);
 	}
-	
+
 	handleForwardClick(o) {
 		this.props.onForwardClick(o);
 	}
-	
+
 	render () {
 		//compare future date with current and decide whether to render end button.
-		//if (changeDate(this.props.timeCode,this.props.endDate,1) > 
-		
+		//if (changeDate(this.props.timeCode,this.props.endDate,1) >
+
 		var dateText;
 
 		switch (this.props.timeCode){
@@ -769,7 +565,7 @@ class DateSelector extends React.Component {
 				console.log("Wrong timecode in DataSelector");
 				break;
 		}
-		
+
 		return (
 			<div className='date-selector-container'>
 				<Button variant="primary" onClick={this.handleBackClick}>{"<"}</Button>
@@ -786,16 +582,16 @@ function GetMonthText(monthIndex){
 }
 
 class TimeMenu extends React.Component {
-	
+
 	constructor(props){
 		super(props)
 		this.handleClick = this.handleClick.bind(this)
 	}
-	
+
 	handleClick(o) {
 		this.props.onClick(o);
 	}
-	
+
 	renderButton(n, v){
 		return (
 			<MenuButton
@@ -804,7 +600,7 @@ class TimeMenu extends React.Component {
 			/>
 		);
 	}
-	
+
 	render () {
 		return (
 			<div className='time-menu-container'>
@@ -830,16 +626,16 @@ function MenuButton(props) {
 }
 
 class GraphMenu extends React.Component {
-	
+
 	constructor(props){
 		super(props)
 		this.handleClick = this.handleClick.bind(this)
 	}
-	
+
 	handleClick(o) {
 		this.props.onClick(o);
 	}
-	
+
 	renderButton(n, v){
 		return (
 			<MenuButton
@@ -848,7 +644,7 @@ class GraphMenu extends React.Component {
 			/>
 		);
 	}
-	
+
 	render () {
 		return (
 			<div className='graph-menu-container'>
@@ -878,7 +674,7 @@ export function changeDate(timecode, adate, modifier) {
 				/* Month */
 				return new Date(d.setMonth(d.getMonth() + (1 * modifier)));
 				//return new Date(d.setMonth(d.getMonth() + (1 * modifier)));
-			default: 
+			default:
 				/* wrong */
 				console.log("WRONG TIMECODE");
 				return d;
@@ -888,8 +684,8 @@ export function changeDate(timecode, adate, modifier) {
 function formatDate(adate) {
 	var d = new Date(adate);
 	return (
-		('0' + d.getDate()).slice(-2) + '/' + 
-		('0' + (d.getMonth() + 1)).slice(-2) + '/' + 
+		('0' + d.getDate()).slice(-2) + '/' +
+		('0' + (d.getMonth() + 1)).slice(-2) + '/' +
 		d.getFullYear()
 	);
 }
@@ -934,7 +730,7 @@ function GetModeEmotion(emotionArray){
 	let dbEmotions = ['anger','contempt','disgust','fear','happiness','neutral','sadness','surprise'];
 	let emotionCount = new Array(dbEmotions.length).fill(0);
 	let i = 0, j = 0;
-	
+
 	for (i = 0; i < emotionArray.length; i++){
 		for (j = 0; j < dbEmotions.length; j++){
 			if (emotionArray[i].includes(dbEmotions[j])){
@@ -942,7 +738,7 @@ function GetModeEmotion(emotionArray){
 			}
 		}
 	}
-	
+
 	//find the max
 	let max = emotionCount[0];
 	let maxID = 0;
@@ -963,17 +759,17 @@ export function GetEmotionIndex(emo){
 			return i;
 		}
 	}
-	
+
 	return -1;
 }
 
 export function convertStringToDate(str){
 	let d = new Date(str);
 	// add time zone offset
-	d.setTime( d.getTime() + d.getTimezoneOffset()*60*1000 )
-    return new Date(d);
+	//d.setTime( d.getTime() + d.getTimezoneOffset()*60*1000 )
+    return d;
 }
-	
+
 export function getUnitQuantity(adate,dateUnit){
 	switch (dateUnit){
 		case 0:
