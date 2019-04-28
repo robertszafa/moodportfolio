34b17320dc9aec0a35c6e065bef421ca843c03aa
rahul-ki <rahul.kothari.201@gmail.com>
2019-04-28 22:19:00 +0100

    fixed various AdminPage.js issues.
    I can;t run the front-end locally rn. Can someone check to see if AdminPage works finally?
    Still can add so many queries to this!

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -5,166 +5,166 @@ import Login from './Login'
 import {apiMoodportfolio} from '../App'
 
 
-export default class Home extends React.Component {
-    constructor(props) {
-      super(props);
-    }
+export default class AdminPage extends React.Component {
+	constructor(props) {
+		super(props);
+	  
+		this.state = {
+			numOfUsers: null,
+			mostPopularTag: null,
+			mostPopularLoc: null,
+			numOfPhotos: null,
+			error: null			
+        }
+		
+		this.getNumberOfUsers = this.getNumberOfUsers.bind(this);
+        this.getMostPopularTag = this.getMostPopularTag.bind(this);
+        this.getMostPopularLoc = this.getMostPopularLoc.bind(this);
+        this.onUploadDescription = this.onUploadDescription.bind(this);
+	}
 
-    // componentWillMount(){
-    //   getNumberOfUsers();
-    //   getMostPopularTag();
-    //   getMostPopularLoc();
-    // }
+	componentWillMount(){
+		this.getNumberOfUsers();
+		this.getMostPopularTag();
+		this.getMostPopularLoc();
+		this.getPhotocountOverLastWeek();
+	}
 
-    getNumberOfUsers() {
-        let authToken = localStorage.getItem("authToken");
+	getNumberOfUsers() {
+		
+		let authToken = localStorage.getItem("authToken");
 
-        fetch(apiMoodportfolio + '/AdminQuery', {
-            method: "GET",
-            mode: "cors",
-            cache: "no-cache",
-            withCredentials: true,
-            credentials: "same-origin",
-            headers: {
-                "Authorization": authToken,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                                  'basedOn': "any",
-                                  'splSQLQuery' : " SELECT COUNT(DISTINCT userID) FROM User",
-                                })
-        })
-        .then((res) => res.json())
-        .then(json => {
-            console.log(json)
-            if(json.success){
-                //result is the answer.
-                this.setState({numOfUsers: json.result})
-            } else {
-                this.setState({error: json.error})
-            }
-        })
-    }
+		fetch(apiMoodportfolio + '/SplAdminQuery', {
+			method: "GET",
+			mode: "cors",
+			cache: "no-cache",
+			withCredentials: true,
+			credentials: "same-origin",
+			headers: {
+				"Authorization": authToken,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+								  'basedOn': "#users",
+								  'splSQLQuery' : ""
+								})
+		})
+		.then((res) => res.json())
+		.then(json => {
+			console.log(json)
+			if(json.success){
+				this.setState({numOfUsers: json.result});
+			} else {
+				this.setState({error: json.error});
+			}
+		})
+	}
 
-    //return the most popular tag from the TAG table
-    getMostPopularTag() {
-        let authToken = localStorage.getItem("authToken");
+	//return the most popular tag from the TAG table
+	getMostPopularTag() {
+		let authToken = localStorage.getItem("authToken");
 
-        fetch(apiMoodportfolio + '/AdminQuery', {
-            method: "GET",
-            mode: "cors",
-            cache: "no-cache",
-            withCredentials: true,
-            credentials: "same-origin",
-            headers: {
-                "Authorization": authToken,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                                  'basedOn': "any",
-                                  'splSQLQuery' : " SELECT name, MAX(count) from Tag",
-                                })
-        })
-        .then((res) => res.json())
-        .then(json => {
-            console.log(json)
-            if(json.success){
-                //result is the answer.
-                this.setState({mostPopularTag: json.result})
-            } else {
-                this.setState({error: json.error})
-            }
-        })
-    }
+		fetch(apiMoodportfolio + '/SplAdminQuery', {
+			method: "GET",
+			mode: "cors",
+			cache: "no-cache",
+			withCredentials: true,
+			credentials: "same-origin",
+			headers: {
+				"Authorization": authToken,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+								  'basedOn': "popularTag",
+								  'splSQLQuery' : ""
+								})
+		})
+		.then((res) => res.json())
+		.then(json => {
+			console.log(json)
+			if(json.success){
+				//result is the answer.
+				this.setState({mostPopularTag: json.result});
+			} else {
+				this.setState({error: json.error});
+			}
+		})
+	}
 
-    //return the most popular location users are from
-    getMostPopularLoc() {
-        let authToken = localStorage.getItem("authToken");
+	//return the most popular location users are from
+	getMostPopularLoc() {
+		let authToken = localStorage.getItem("authToken");
 
-        fetch(apiMoodportfolio + '/AdminQuery', {
-            method: "GET",
-            mode: "cors",
-            cache: "no-cache",
-            withCredentials: true,
-            credentials: "same-origin",
-            headers: {
-                "Authorization": authToken,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                                  'basedOn': "any",
-                                  'splSQLQuery' : " SELECT country, MAX(country) from User",
-                                })
-        })
-        .then((res) => res.json())
-        .then(json => {
-            console.log(json)
-            if(json.success){
-                //result is the answer.
-                this.setState({mostPopularLoc: json.result})
-            } else {
-                this.setState({error: json.error})
-            }
-        })
-    }
+		fetch(apiMoodportfolio + '/SplAdminQuery', {
+			method: "GET",
+			mode: "cors",
+			cache: "no-cache",
+			withCredentials: true,
+			credentials: "same-origin",
+			headers: {
+				"Authorization": authToken,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+								  'basedOn': "any",
+								  'splSQLQuery' : "SELECT country, MAX(country) from User",
+								})
+		})
+		.then((res) => res.json())
+		.then(json => {
+			console.log(json)
+			if(json.success){
+				//result is the answer.
+				this.setState({mostPopularLoc: json.result});
+			} else {
+				this.setState({error: json.error});
+			}
+		})
+	}
 
+	//return the photos uploaded per day
+	getPhotocountOverLastWeek() {
+		let authToken = localStorage.getItem("authToken");
 
-
-    //return the photos uploaded per day
-    getPhotocountOverLastWeek() {
-        let authToken = localStorage.getItem("authToken");
-
-        fetch(apiMoodportfolio + '/AdminQuery', {
-            method: "GET",
-            mode: "cors",
-            cache: "no-cache",
-            withCredentials: true,
-            credentials: "same-origin",
-            headers: {
-                "Authorization": authToken,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                                  'basedOn': "any",
-                                  'splSQLQuery' : " SELECT Date(), count(photoID) FROM Photo GROUP BY DATE();",
-                                })
-        })
-        .then((res) => res.json())
-        .then(json => {
-            console.log(json)
-            if(json.success){
-                //result is the answer.
-                this.setState({mostPopularTag: json.result})
-            } else {
-                this.setState({error: json.error})
-            }
-        })
-    }
+		fetch(apiMoodportfolio + '/AdminQuery', {
+			method: "GET",
+			mode: "cors",
+			cache: "no-cache",
+			withCredentials: true,
+			credentials: "same-origin",
+			headers: {
+				"Authorization": authToken,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+								  'basedOn': "any",
+								  'splSQLQuery' : "SELECT Date(timestamp), count(photoID) FROM Photo GROUP BY DATE(timestamp)",
+								})
+		})
+		.then((res) => res.json())
+		.then(json => {
+			console.log(json)
+			if(json.success){
+				//result is the answer.
+				this.setState({numOfPhotos: json.result})
+			} else {
+				this.setState({error: json.error})
+			}
+		})
+	}
 
 
-    //Render web page
-    render() {
-      return (
-          <div className = "text-center homePageContainer">
-            <br></br>
-            <br></br>
-            <div>
-              <p>Number of users registered : </p>
-
-            </div>
-            <div>
-                <p>Most popular tag: </p>
-            </div>
-            <div>
-                <p>Most popular location : </p>
-            </div>
-            <div className = "userList">
-                <p>List of users</p>
-            </div>
-            <div className = "photosOverTime">
-                <p>How many photos have been uploaded over the last week</p>
-            </div>
-          </div>
-      );
-    }
-  }
+	//Render web page
+	render() {
+	  return (
+		  <div className = "text-center homePageContainer">
+			<br></br>
+			<br></br>
+			<p>Number of users registered : {this.state.numOfUsers}</p>
+			<p>Most popular tag: {this.state.mostPopularTag}</p>			
+			<p>Most popular location : {this.state.mostPopularLoc}</p>
+			<p>How many photos have been uploaded over the last week : {this.state.numOfPhotos}</p>
+		  </div>
+	  );
+	}
+}
